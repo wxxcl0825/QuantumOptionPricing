70259ed62cba6538dd098d727a9783efe4e32a3e
Chenli Xue <wxxcl0825@126.com>
2023-09-06 18:37:51 +0800

    Changes

--- view/MainWindow.py
+++ view/MainWindow.py
@@ -1,3 +1,4 @@
+from PyQt5.QtGui import QFontDatabase
 from PyQt5.QtWidgets import QMainWindow
 
 from ui.MainWindow import Ui_MainWindow
@@ -5,6 +6,11 @@ from ui.MainWindow import Ui_MainWindow
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
+        self.load_fonts()
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+
+    def load_fonts(self):
+        QFontDatabase().addApplicationFont(":fonts/fonts/hitmo.ttf")
+        QFontDatabase().addApplicationFont(":fonts/fonts/rostov.ttf")
